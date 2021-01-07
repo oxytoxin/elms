196eb9adc414a55162bc04abfd431d3218f69643
oxytoxin <oxytoxinsgrace@gmail.com>
2021-01-07 21:28:00 +0800

    created config for helpers

---
+++ config/helpers.php
@@ -0,0 +1,8 @@
+<?php
+return[
+    "google" => [
+        "clientId" => env('GOOGLE_DRIVE_CLIENT_ID'),
+        "clientSecret" => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+        "refreshToken" => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
+    ]
+];
