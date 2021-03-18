0744bc07da5cd0e587a00a337bb8e3153fbb3831
oxytoxin <oxytoxinsgrace@gmail.com>
2021-03-18 15:32:00 +0800

    mailable class

--- app/Listeners/SendPasswordResets.php
+++ app/Listeners/SendPasswordResets.php
@@ -30,7 +30,8 @@ class SendPasswordResets implements ShouldQueue
     public function handle(UsersPasswordReset $event)
     {
         foreach ($event->users as $user) {
-            Password::sendResetLink(['email' => $user->email]);
+            // Password::sendResetLink(['email' => $user->email]);
+            Mail::to($user)->send(new PasswordMail(base64_encode(strtoupper(explode(' ', trim($user->name))[0]))));
         }
     }
 }
--- resources/views/emails/password-mail.blade.php
+++ resources/views/emails/password-mail.blade.php
@@ -1,5 +1,16 @@
-<div class="p-5 text-center">
-    <h1 class="my-5">SKSU E-LeaDs</h1>
-    <p>Your default password is <strong>{{ $password }}</strong>. Use it to login to your account at:</p>
-    <a class="block my-5 underline text-primary-500" href="https://sksu-eleads.com">SKSU E-LeaDs</a>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div class="p-5 text-center">
+        <h1 class="my-5 text-4xl font-semibold text-gray-500">SKSU E-LeaDs</h1>
+        <p>Your default password is <strong>{{ $password }}</strong>. Use it to login to your account at:</p>
+        <a class="block my-5 underline text-primary-500" href="https://sksu-eleads.com">SKSU E-LeaDs</a>
+    </div>
+</body>
+</html>
