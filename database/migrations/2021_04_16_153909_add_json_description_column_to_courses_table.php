64a9362cc02b602875a4ed777483b0037ed11cd1
oxytoxin <oxytoxinsgrace@gmail.com>
2021-04-16 07:44:20 +0000

    added json column to courses table for name conflicts

---
+++ database/migrations/2021_04_16_153909_add_json_description_column_to_courses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddJsonDescriptionColumnToCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->json('name_conflict')->after('name')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            //
+        });
+    }
+}
